1d0fd5c31f2ab7903c37b77fc4f87e41ee4df6c8
Pom <surapol.kun@gmail.com>
2023-11-16 12:15:23 +0700

    update css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,18 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    extend: {},
+    screens: {
+      'desktop': '1920px',
+      'tablet': '768px',
+      'mobile': '320px'
     },
+    colors: {
+      'white': '#FFFFFF',
+      'gray': '#F5F4F9',
+      'purple': '#5E3DB3',
+      'darkblue': '#090C35'
+    }
   },
   plugins: [],
 }
